fe42cfb89740fe06f27d07ce77ceda9a79a08a8d
ankitbhat <63880287+ankitbhat@users.noreply.github.com>
2020-05-12 01:42:43 +0530

    Update empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,19 +5,14 @@ wageperhour=20;
 fullday=8
 dailywage=$(( $wageperhour * $fullday ));
 salary=0;
-counter=0;
-for (( count=0; count<=10; count++ ))
-
-do
-	status=$(( $RANDOM%2))
-	if [ $status -eq $ispresent ]
-	then
-		echo "Employee is present"
-		salary=$(( $salary+$dailywage));
-		counter=$(( $counter+1 ));
-	else
-		echo "Employee is absent"
-		salary=$(($salary+0));
-	fi
+status=$(( $RANDOM%2))
+if [ $status -eq $ispresent ]
+then
+	echo "Employee is present"
+	salary=$(( $salary+$dailywage));
+else
+	echo "Employee is absent"
+	salary=$(($salary+0));
+fi
 echo $salary
 done
