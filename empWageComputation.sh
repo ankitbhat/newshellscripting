94d323f45a4a79c6875256a027271130915a321e
ankitbhat <63880287+ankitbhat@users.noreply.github.com>
2020-05-12 01:40:46 +0530

    Update empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -x
 echo Welcome to Employee Wage Computation Program
 ispresent=1;
-for (( count=0; count<=10; count++ ))
-
-do
-	status=$(( $RANDOM%2))
-	if [ $status -eq $ispresent ]
-	then
-		echo "Employee is present"
+status=$(( $RANDOM%2))
+if [ $status -eq $ispresent ]
+then
+	echo "Employee is present"
 else
-		echo "Employee is absent"
+	echo "Employee is absent"
 	fi
 done
