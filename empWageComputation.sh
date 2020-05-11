70885e9a48b629a8ba2bcc790d389472d7317aaf
ankitbhat <63880287+ankitbhat@users.noreply.github.com>
2020-05-12 01:44:50 +0530

    Update empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,20 +5,16 @@ isfulltime=2;
 wageperhour=20;
 dailywage=$(( $wageperhour * 8 ));
 salary=0;
-counter=0;
-for (( count=0; count<=10; count++ ))
-
-do
-	status=$(( $RANDOM%3))
-	if [ $status -eq $isparttime ]
-	then
-		echo "Employee is present and part time"
-	elif [ $status -eq $isfulltime ]
-	then
-                echo "Employee is present and full time"
-	else
-		echo "Employee is absent"
-		salary=$(($salary+0));
-	fi
+status=$(( $RANDOM%3))
+if [ $status -eq $isparttime ]
+then
+	echo "Employee is present and part time"
+elif [ $status -eq $isfulltime ]
+then
+        echo "Employee is present and full time"
+else
+	echo "Employee is absent"
+	salary=$(($salary+0));
+fi
 echo $salary
 done
